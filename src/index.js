a1ed1dbeeec7cb4af9057a57ea408a1799ba6969
Darrell <webtastic.dc@gmail.com>
2017-11-12 19:00:03 -0800

    Added YouTube Search API to Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY =  'AIzaSyCFUMwqxCM3Fq2J8CQG4iogZGn94vOPcPo'; 
+
+
 //Create a new component.  This component should
 //produce some HTML
 const App = function() {
@@ -7,3 +13,4 @@ const App = function() {
 
 // Take this component's generated HTML and put it
 //on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
