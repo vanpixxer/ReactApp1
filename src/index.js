7c065d5ec674042cd123ddd3c3d6147e4a4e8c63
Darrell <webtastic.dc@gmail.com>
2017-11-19 18:15:15 -0800

    Learned Meaning of Props, Worked with Map Iterator, and List Key Items

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY =  'AIzaSyBJ8BSrjCG8PuCNIyOHVi9JyJsrrpev9KM';
 
 //Create a new component.  This component should
@@ -20,6 +21,7 @@ render() {
   return (
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>
    );
  }
