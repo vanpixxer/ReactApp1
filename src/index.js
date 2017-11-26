410242dc21cab05c84aa5b2feb7c010d4292ffbd
Darrell <webtastic.dc@gmail.com>
2017-11-25 18:22:11 -0800

    The App can now load YouTube videos and play one of them

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY =  'AIzaSyBJ8BSrjCG8PuCNIyOHVi9JyJsrrpev9KM';
 
 //Create a new component.  This component should
@@ -13,7 +14,7 @@ constructor(props) {
 
   this.state = { videos: [] };
 
-  YTSearch({key: API_KEY, term: 'surfboards'},  (videos)  => {
+  YTSearch({key: API_KEY, term: 'street photography'},  (videos)  => {
     this.setState( { videos });
   });
 }
@@ -21,7 +22,8 @@ render() {
   return (
     <div>
       <SearchBar />
-      <VideoList videos={this.state.videos} />
+      <VideoDetail video={this.state.videos[0]} />
+      <VideoList videos={this.state.videos}/>
     </div>
    );
  }
